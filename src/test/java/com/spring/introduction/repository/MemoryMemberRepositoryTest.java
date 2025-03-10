e6bc63b18a515205dbf59172b21d202a445b38d5
SYAAINN <alswo08319@gmail.com>
2025-03-10 16:28:10 +0900

    feat : domain, repository, service 에 대한 간단한 예제를 구현하고, 테스트 코드 작성을 연습합니다.

---
+++ src/test/java/com/spring/introduction/repository/MemoryMemberRepositoryTest.java
@@ -0,0 +1,61 @@
+package com.spring.introduction.repository;
+
+import com.spring.introduction.domain.Member;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class MemoryMemberRepositoryTest {
+
+    MemoryMemberRepository repository = new MemoryMemberRepository();
+
+    @AfterEach
+    public void afterEach() { // 각 테스트 메소드 진행 후 클리어해서 독립적으로 실행될 수 있도록 함. 중요!!
+        repository.clearStore();
+    }
+
+    @Test
+    public void save() {
+        Member member = new Member();
+        member.setName("spring");
+
+        repository.save(member);
+
+        Member result = repository.findById(member.getId()).get();
+        assertThat(member).isEqualTo(result);
+    }
+
+    @Test
+    public void findByName() {
+        Member member1 = new Member();
+        member1.setName("spring1");
+        repository.save(member1);
+
+        Member member2 = new Member();
+        member2.setName("spring2");
+        repository.save(member2);
+
+        Member result = repository.findByName("spring1").get();
+
+        assertThat(result).isEqualTo(member1);
+    }
+
+    @Test
+    public void findAll() {
+        Member member1 = new Member();
+        member1.setName("spring1");
+        repository.save(member1);
+
+        Member member2 = new Member();
+        member2.setName("spring2");
+        repository.save(member2);
+
+        List<Member> result = repository.findAll();
+
+        assertThat(result.size()).isEqualTo(2);
+
+    }
+}
