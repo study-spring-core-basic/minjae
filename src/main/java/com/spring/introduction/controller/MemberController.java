3348102fcf154c7938db19a15244f09d0524519a
SYAAINN <alswo08319@gmail.com>
2025-03-14 16:24:34 +0900

    feat : 섹션6 실습을 진행합니다. (웹 MVC 개발과 회원등록/조회 기능 실습)

--- src/main/java/com/spring/introduction/controller/MemberController.java
+++ src/main/java/com/spring/introduction/controller/MemberController.java
@@ -1,8 +1,14 @@
 package com.spring.introduction.controller;
 
+import com.spring.introduction.domain.Member;
 import com.spring.introduction.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
 
 @Controller // 1. 컨트롤러 객체 생성해서 스프링 컨테이너에 삽입하는 역할을 자동으로 수행하는 어노테이션
 public class MemberController {
@@ -12,5 +18,27 @@ public class MemberController {
     @Autowired // 2. 스프링 컨테이너에 있는 MemberService 객체를 가져다 연결해주는 역할
     public MemberController(MemberService memberService) {
         this.memberService = memberService;
+    }
+
+    @GetMapping("/members/new")
+    public String createForm() {
+        return "members/createMemberForm";
+    }
+
+    @PostMapping("members/new")
+    public String create(MemberForm form) {
+        Member member = new Member();
+        member.setName(form.getName());
+
+        memberService.join(member);
+
+        return "redirect:/";
+    }
+
+    @GetMapping("/members")
+    public String list(Model model) {
+        List<Member> members = memberService.findMembers();
+        model.addAttribute("members", members);
+        return "members/MemberList";
     }
 }
