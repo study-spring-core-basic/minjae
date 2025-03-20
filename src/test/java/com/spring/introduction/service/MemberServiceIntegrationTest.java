763145505ecdc5aebeae8409faa083cf73796459
SYAAINN <alswo08319@gmail.com>
2025-03-20 11:05:31 +0900

    feat : 섹션7 실습을 진행합니다. (jdbc 템플릿을 이용해 데이터베이스 접근 과정을 간략하게 개선합니다.)

---
+++ src/test/java/com/spring/introduction/service/MemberServiceIntegrationTest.java
@@ -0,0 +1,55 @@
+package com.spring.introduction.service;
+
+import com.spring.introduction.domain.Member;
+import com.spring.introduction.repository.MemberRepository;
+import com.spring.introduction.repository.MemoryMemberRepository;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+@SpringBootTest //
+@Transactional // 테스트 하고 나서 종료될 때 다 삭제해서 초기상태로 롤백 해줌.
+class MemberServiceIntegrationTest {
+
+    @Autowired MemberService memberService;
+    @Autowired MemberRepository memberRepository;
+
+    @Test
+    void join() {
+        // given
+        Member member = new Member();
+        member.setName("spring");
+
+        // when
+        Long saveId = memberService.join(member);
+
+        // then
+        Member findMember = memberService.findOne(saveId).get();
+        assertThat(member.getName()).isEqualTo(findMember.getName());
+    }
+
+    @Test
+    public void joinDuplicateException() {
+        // given
+        Member member1 = new Member();
+        member1.setName("spring");
+
+        Member member2 = new Member();
+        member2.setName("spring");
+
+        // when
+        memberService.join(member1);
+        IllegalStateException e = assertThrows(IllegalStateException.class, () -> memberService.join(member2));
+
+        assertThat(e.getMessage()).isEqualTo("이미 존재하는 회원입니다.");
+
+        // then
+    }
+
+}
